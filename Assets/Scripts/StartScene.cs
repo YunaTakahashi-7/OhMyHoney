faa25d6e1f92668928f02fd97db2eedf9eba6cf7
Yuna Takahashi <y.takahashi99624@gmail.com>
2021-02-27 19:23:58 +0900

    build2

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+    public GameObject StartTimeline;
+    public GameObject SelectSceneButton;
+
+    public void StartMovie()
+    {
+        StartTimeline.SetActive(true);
+    }
+    public void SelectScene()
+    {
+        SelectSceneButton.SetActive(true);
+    }
+}
